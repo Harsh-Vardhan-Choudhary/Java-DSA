6165c34d50f4676b9ec58f9183d8ce98f645f022
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2023-09-30 21:57:25 +0530

    array list done + Linked list started

--- ArrayList/PairSum2.java
+++ ArrayList/PairSum2.java
@@ -5,8 +5,60 @@ import java.util.ArrayList;
 
 public class PairSum2
 {
+    public static boolean pairsum(ArrayList<Integer> pair, int target)
+    {
+        int bp = -1;        //breaking point
+        for(int i=0; i<pair.size(); i++)
+        {
+            if(pair.get(i) > pair.get(i+1))
+            {
+                bp = i;
+                break;
+            }
+        }
+
+        int LP = bp+1;
+        int RP = bp;
+
+        while(LP != RP)
+        {
+            if(pair.get(LP) + pair.get(RP) == target)
+            {
+                return true;
+            }
+
+            else if(pair.get(LP) + pair.get(RP) < target)
+            {
+                LP = (LP+1)%pair.size();
+            }
+            
+            else
+            {
+                RP = (RP - 1 + pair.size())%pair.size();
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args)
     {
         ArrayList<Integer> pair = new ArrayList<>();
+        pair.add(11);
+        pair.add(15);
+        pair.add(6);
+        pair.add(8);
+        pair.add(9);
+        pair.add(10);   
+        
+        int target = 16;
+
+        if(pairsum(pair, target) == true)
+        {
+            System.out.println("Target found");
+        }
+        else
+        {
+            System.out.println("Invalid target");
+        }
     }    
 }
---
+++ Linked List/LinkedList.java
@@ -0,0 +1,46 @@
+//package Linked List;
+
+public class LinkedList
+{
+    public static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data)
+    {
+        //step1 = create new node
+        Node newNode = new Node(data);
+
+        //if 0 node in a linked list
+        if(head == null)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        //step2 - newNode next = head
+        newNode.next = head;
+
+        //step3 - head = newNode
+        head = newNode;
+
+    }
+
+    public static void main(String args[])
+    {
+        LinkedList ll = new LinkedList();
+    }
+
+
+}
