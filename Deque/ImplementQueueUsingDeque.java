d4de76405a3661e96c209dc3d7fa07da0c05b1bd
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2023-10-28 09:50:27 +0530

    Deque

---
+++ Deque/ImplementQueueUsingDeque.java
@@ -0,0 +1,39 @@
+package Deque;
+import java.util.*;
+
+public class ImplementQueueUsingDeque 
+{
+    static class Queue
+    {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void add(int data)
+        {
+            deque.addLast(data);
+        }
+
+        public int remove()
+        {
+            return deque.removeFirst();
+        }
+
+        public int front()
+        {
+            return deque.getFirst();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("front" + q.front());
+        
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    }
+}
