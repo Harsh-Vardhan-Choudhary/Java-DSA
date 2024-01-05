bbd68d0c693be1f6a08089b381ad5e18a1edd423
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-01-05 22:19:19 +0530

    TopViewTree

--- Binary Trees/TopViewTree.java
+++ Binary Trees/TopViewTree.java
@@ -34,8 +34,60 @@ public class TopViewTree
         //level order traversal
 
         Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max =0;
+
+        q.add(new Info(root, 0));
+        q.add(null);
+
+        while(!q.isEmpty())
+        {
+            Info curr = q.remove();
+
+            if(curr == null)
+            {
+                if(q.isEmpty())
+                {
+                    break;
+                }
+                else
+                {
+                    q.add(null);
+                }
+            }
+
+            else
+            {
+                if(!map.containsKey(curr.hd))   //first time hd is occuring
+                {
+                    map.put(curr.hd, curr.node);
+                }
+
+                if(curr.node.left != null)
+                {
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min = Math.min(min, curr.hd-1);
+                }
+
+                if(curr.node.right != null)
+                {
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max = Math.max(max, curr.hd+1);
+                }                
+            }
+
+        }
+
+        for(int i=min; i<=max; i++)
+        {
+            System.out.print(map.get(i).data + "\t");
+        }
+        System.out.println();
 
     }
+
+
     public static void main(String args[])    
     {
         /*
@@ -54,6 +106,8 @@ public class TopViewTree
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
+
+        topView(root);
     }
     
 }
