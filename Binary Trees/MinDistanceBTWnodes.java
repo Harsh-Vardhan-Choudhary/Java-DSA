6769e1bb9f917025a6f0b60e2b9b0a2fe34b9c50
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-01-08 02:38:21 +0530

    MinDistanceBTWnodes

--- Binary Trees/MinDistanceBTWnodes.java
+++ Binary Trees/MinDistanceBTWnodes.java
@@ -13,6 +13,78 @@ public class MinDistanceBTWnodes
             this.right = null;
         }
     }
+
+    public static Node lca2(Node root, int n1, int n2)
+    {
+        if(root == null)
+        {
+            return root;
+        }
+
+        if(root.data == n1 || root.data == n2)
+        {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        //leftLCA = valid | rightLCA = null
+        if(rightLca == null)
+        {
+            return leftLca;
+        }
+
+        //leftLCA = null | rightLCA = valid
+        if(leftLca == null)
+        {
+            return rightLca;
+        }
+
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n)
+    {
+        if(root == null)
+        {
+            return -1;
+        }
+
+        if(root.data == n)
+        {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1)
+        {
+            return -1;
+        }
+        else if(leftDist == -1)
+        {
+            return rightDist+1;
+        }
+        else
+        {
+            return leftDist+1;
+        }
+
+
+
+    }
+
+    public static int minDist(Node root, int n1, int n2)
+    {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1+dist2;
+    }
+    
     public static void main(String args[])    
     {
         /*
@@ -32,5 +104,7 @@ public class MinDistanceBTWnodes
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
+        int n1 = 4, n2 = 6;
+        System.out.println("Minimum distance is : " + minDist(root, n1, n2));
     }    
 }
