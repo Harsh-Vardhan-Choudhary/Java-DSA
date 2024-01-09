40b204ac8fd59c54eec84937f345411fbe286d28
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-01-09 08:33:43 +0530

    BST

--- Binary Search Tree/BST.java
+++ Binary Search Tree/BST.java
@@ -1,5 +1,7 @@
 //package Binary Search Tree;
 
+import java.util.ArrayList;
+
 public class BST
 {
     static class Node
@@ -149,6 +151,53 @@ public class BST
         }
         
     }
+
+    public static void printPath(ArrayList<Integer> path)
+    {
+        for(int i=0; i<path.size(); i++)
+        {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("null");
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        path.add(root.data);
+
+        if(root.left == null && root.right == null)
+        {
+            printPath(path);
+        }
+
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
+
+    public static boolean isVlidBST(Node root,Node min, Node max)
+    {
+        if(root == null)
+        {
+            return true;
+        }
+
+        if(min != null && root.data <= min.data)
+        {
+            return false;
+        }
+        else if(max != null && root.data >= max.data)
+        {
+            return false;
+        }
+
+        return isVlidBST(root.left, min, root) && isVlidBST(root.right, root, max);
+    }
     
     public static void main(String args[])
     {
@@ -177,6 +226,17 @@ public class BST
 
         // inorder(root);
 
-        printInRange(root, 5, 8);
+        //printInRange(root, 5, 8);
+
+        //printRoot2Leaf(root, new ArrayList<>());
+
+        if(isVlidBST(root, null, null))
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+            System.out.println("Invalid");
+        }
     }
 }
