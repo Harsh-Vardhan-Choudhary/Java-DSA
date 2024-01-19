3d22e09f17ededa260c2bc85fd560aac7b81606b
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-01-19 19:21:57 +0530

    heap implimentation

--- Heap/heap.java
+++ Heap/heap.java
@@ -8,6 +8,7 @@ public class heap
     {
         ArrayList<Integer> arr = new ArrayList<>();
 
+        //insert in heap
         public void add(int data)   //O(logn)
         {
             arr.add(data);
@@ -21,15 +22,83 @@ public class heap
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x = par;
+                par = (x-1)/2;
             }
         }
 
+        //peek in heap
         public int peek()
         {
             return arr.get(0);
         }
 
-        
+        //heapify is a private function
+        private void heapify(int i)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            int minIdx = i;
 
+            if(left < arr.size() && arr.get(minIdx) > arr.get(left))
+            {
+                minIdx = left;
+            }
+
+            if(right < arr.size() && arr.get(minIdx) > arr.get(right))
+            {
+                minIdx = right;
+            }
+
+            if(minIdx != i)
+            {
+                //swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp); 
+
+                heapify(minIdx);
+            }
+        }
+
+        //delete in heap
+        public int remove()
+        {
+            int data = arr.get(0);
+
+            //step1 - swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            //step2 - delete last
+            arr.remove(arr.size()-1);
+
+            //step3 - heapify
+            heapify(0);
+            return data;
+        }
+
+        //heap is empty
+        public boolean isEmpty()
+        {
+            return arr.size() == 0;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        HEAP h = new HEAP();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while(!h.isEmpty())
+        {
+            System.out.print(h.peek());
+            h.remove();
+        }
     }
 }
