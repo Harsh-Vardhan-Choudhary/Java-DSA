92e6d2699b3243719e691156a6ea839fb6015fd3
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-01-31 17:00:56 +0530

    Union_Intersection

---
+++ Hashmap/Union_Intersection.java
@@ -0,0 +1,48 @@
+package Hashmap;
+
+import java.util.*;
+
+public class Union_Intersection 
+{
+    public static void main(String[] args)
+    {
+        int arr1[] = {7,3,9};
+        int arr2[] = {6,3,9,1,9,4};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        //union
+        for(int i=0; i<arr1.length; i++)
+        {
+            set.add(arr1[i]);
+        }
+        for(int i=0; i<arr2.length; i++)
+        {
+            set.add(arr2[i]);
+        }
+
+        System.out.println("union = " + set.size());
+
+
+        set.clear();
+        //Intersection
+        for(int i=0; i<arr1.length; i++)
+        {
+            set.add(arr1[i]);
+        }
+
+        int count=0;
+
+        for(int i=0; i<arr2.length; i++)
+        {
+            if(set.contains(arr2[i]))
+            {
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+
+        System.out.println("Intersection = " + count);
+
+    }    
+}
