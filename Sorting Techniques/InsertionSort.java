b8bef143271d17bbb8292d08c8885a523c4f0a77
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-07-21 04:16:16 +0530

    IS

--- Sorting Techniques/InsertionSort.java
+++ Sorting Techniques/InsertionSort.java
@@ -1,46 +1,32 @@
 //package Sorting Techniques;
 
-import java.util.Scanner;
-
 public class InsertionSort
 {
-    public static void InsertionSort(int array[]) 
-    {
-        for(int i=1; i<array.length; i++)
-        {
-            int curr = array[i];
-            int prev = i-1;
-
-            //finding out the curent pos to insert
-            while(prev >= 0 && array[prev] > curr)
-            {
-                array[prev+1] = array[prev];
-                prev--;
+    public static void IS(int arr[]) {
+        for(int i = 1; i < arr.length; i++) {
+            int j = i;
+            while(j > 0 && arr[j - 1] > arr[j]) {
+                swap(arr, j, j - 1);
+                j--;
             }
-
-            //insertion
-            array[prev+1] = curr;
         }
     }
-
-    public static void main(String args[])   
-    {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Enter the size of the array : ");
-        int n = input.nextInt();
-        int array[] = new int[n];
-        for(int i=0; i<array.length; i++)
-        {
-            System.out.print("Enter the value of element at index ["+i+"] : ");
-            array[i] = input.nextInt();
+    
+    public static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public static void printArr(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-
-        InsertionSort(array);
-        System.out.println("Your Sorted array is : ");
-        for(int i=0; i<n; i++)
-        {
-            System.out.print(array[i] + "\t");
-        }
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = {3, 7, 1, 4, 6, 9};
+        IS(arr);
+        printArr(arr);
     }
 }
