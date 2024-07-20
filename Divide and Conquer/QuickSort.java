eec2ad313dbe4c98a36b3868b44c665594805eb0
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-07-21 02:57:33 +0530

    Quick sort

--- Divide and Conquer/QuickSort.java
+++ Divide and Conquer/QuickSort.java
@@ -6,11 +6,11 @@ public class QuickSort {
         int j = high;
 
         while (i<j) {
-            while(arr[i] <= pivot && i<=high) {
+            while(arr[i] <= pivot && i<high) {
                 i++;
             }
 
-            while(arr[j] > pivot && j>=low) {
+            while(arr[j] > pivot && j>low) {
                 j--;
             }
 
@@ -29,7 +29,7 @@ public class QuickSort {
     }
 
     public static void qs(int arr[], int low, int high) {
-        if(low<high) {        //until we get the single element array
+        if(low<high) {        //until we get the single element array | if low==high, then single element array
             int pIndex = Partition(arr, low, high);
             qs(arr, low, pIndex-1);
             qs(arr, pIndex+1, high);
@@ -41,6 +41,7 @@ public class QuickSort {
             System.out.print(arr[i] + " ");
         }
     }
+    
     public static void main(String[] args) {
         int arr[] = {4,6,1,3,8,7};
 
