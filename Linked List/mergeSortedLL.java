2b63f7f348bbcdf5133584b837838a043bfe067d
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-08-18 23:48:05 +0530

    merging two sorted ll

---
+++ Linked List/mergeSortedLL.java
@@ -0,0 +1,35 @@
+public class mergeSortedLL {
+
+    //Definition for singly-linked list
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode(-1);      //work as new head for resulted list
+        ListNode temp = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        //if any element is missing in any of the list
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+        return dummyNode.next;
+    }
+}
