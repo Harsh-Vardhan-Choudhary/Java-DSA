e44c877108b62206660226e539bdc387dff39b1d
Harsh-Vardhan-Choudhary <choudharyharsh964@gmail.com>
2024-09-03 14:43:16 +0530

    Delete middle node in a LL

---
+++ Linked List/DeleteMidNodeLL.java
@@ -0,0 +1,31 @@
+public class DeleteMidNodeLL {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode deleteMiddle(ListNode head) {email
+
+        if (head == null || head.next == null) {
+            return null; // List is empty or has only one node
+        }
+        
+        //find middle node
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast.next.next != null && fast.next.next.next!=null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        //skipping the mid value
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
